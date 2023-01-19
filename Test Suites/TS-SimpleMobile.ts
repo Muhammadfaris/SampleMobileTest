9b63d33c65c448790bda125394d4b02c33bde1e6
Muhammadfaris <imaduddin.muhammadfaris@gmail.com>
2023-01-19 23:58:51 +0700

    Adding Test Suite

---
+++ Test Suites/TS-SimpleMobile.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS-SimpleMobile</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>f3803d5d-bee2-4a04-a873-2313b4540488</testSuiteGuid>
+   <testCaseLink>
+      <guid>bcb66bc6-baeb-48e5-9f77-2e60d610b5c6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC001 - User must be can login with correct credential</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
